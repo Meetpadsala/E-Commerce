728afd95290310abc31a79d07eed28fbb9646553
Meetpadsala <2402031030051@silveroakuni.ac.in>
2026-05-19 16:37:48 +0530

    Create product_seed.py

---
+++ product_seed.py
@@ -0,0 +1,16 @@
+from yourapp.models import Product, Category
+from products_seed import products_seed_data
+
+def run():
+    for item in products_seed_data:
+        category = Category.objects.get(id=item["c_id"])
+
+        Product.objects.create(
+            pname=item["pname"],
+            pprice=item["pprice"],
+            pdis=item["pdis"],
+            pimage=item["pimage"],   # image path string
+            c_id=category
+        )
+
+    print("10 Products Inserted Successfully!")
